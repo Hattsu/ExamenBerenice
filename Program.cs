f32a4cb22359a9fb7a0d7c8fd699e72edf1b1a67
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-06-02 16:05:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// definir un metodo que muestre identidad
+class estudiante
+{
+    public estudiante(¨nombre, apellido, institucion, identidad¨, estudiante estudiante);
+    {
+            public estudiante(estudiante estudianteprimaria)
+           this.estudiante = estudianteprimaria;
+
+    public estudiante(estudiante estudiante) => 
+    this.estudiante = estudiante;
+
+    estudiante = new  class new estudiantesecundaria,
+            estudiante = new estudianteuniversitario;
+    }
+}
